74817c9942622c36832ec3b668300f67d37acb2b
mercaglar <omer.caglar163@hotmail.com>
2023-09-12 22:00:54 +0200

    Updated diagnos 1

--- Diagnos 1.py
+++ Diagnos 1.py
@@ -1,6 +1,6 @@
-def print(n):
+def print2(n):
     if not (1 <= n <= 99):
         print("N måste vara ett heltal mellan 1 och 99.")
     for i in range(2, n + 1):
         print(i)  
-print (99)
+print2 (99)
